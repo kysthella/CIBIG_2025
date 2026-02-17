fec4f6d52dbe0ec008a8708e1afbd018813fcc27
TIBIRI + Ezechiel + titulaire + 2027-09-24 <tibiri@master.cluster>
2026-02-17 18:31:57 +0000

    updated

--- scripts/characterization_script.sh
+++ scripts/characterization_script.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
 #### SLURM configuration #####
-# Define the name  of the job
 #SBATCH --job-name=AMR_1
-# define the partition to use
 #SBATCH -p normal
-# define the number of cpu to use
 #SBATCH --cpus-per-task=16
 #SBATCH --output=AMR_1_%j.out
 #SBATCH --error=AMR_1_%j.err
@@ -18,45 +14,41 @@ set -euo pipefail                      #######exit on error, undefined variables
 STORAGE="storage:/projects/AMRKY/FASTQ_WGS_LABIOGENE/FASTQ"
 ASSEMBLIES="storage:/projects/AMRKY/assemblies"
 WORKPATH="/data/ky/characterization"
+#FASTQ="SQK-RBK114-96_barcode$i.fastq"
+OUTDIR="${WORKPATH}/results"
 mkdir -p ${WORKPATH}
-cd ${WORKPATH}
+#cd ${WORKPATH}
 
 ###########################################
 ##### Modules load
 module load nanoplot/1.42.0
-module load flye/2.9.6-b1802
+module load flye/2.9.6
 module load medaka/2.1.1
-##### 
-for i in {1..39}
+
+cd ${WORKPATH}
+rsync -ravz --progress ${STORAGE} .
+for i in {01..39}
 do
-    BARCODE=barcode$(printf "%02d" $i)
-    FASTQ="SQK-RBK114-96_${BARCODE}.fastq"
-    OUTDIR="${WORKPATH}/results/${BARCODE}"
-    mkdir -p ${OUTDIR}
+    mkdir -p $OUTDIR
+    echo "Analysis  barcode$i"
+    #Go to path directory
+    cd $OUTDIR
+    mkdir -p barcode$i
+    # retreive data from the storage
+    # rsync -ravz --progress ${STORAGE}/${FASTQ} .
+    # Quality Control
+    NanoPlot --fastq ${WORKPATH}/FASTQ/SQK-RBK114-96_barcode$i.fastq -o  barcode$i/qc_report -t 16
+    # Assembly
+    flye --nano-hq ${WORKPATH}/FASTQ/SQK-RBK114-96_barcode$i.fastq -o barcode$i/assembly -t 16 -g 5m --plasmids
+    # Polishing
+    medaka_consensus -i ${WORKPATH}/FASTQ/SQK-RBK114-96_barcode$i.fastq \
+         -d barcode$i/assembly/assembly.fasta \
+         -m r1041_e82_400bps_sup_v5.2.0 \
+         -o barcode$i/medaka_out -t 8
+    ########Transfert the contigs to the storage
+    # rsync -ravz --progress medaka_out/consensus.fasta  ${ASSEMBLIES}/${BARCODE}_contig.fasta
 
-    echo "Analysis  $BARCODE"
-#Go to path directory
-    cd ${OUTDIR}
- 
-# retreive data from the storage
-    rsync -ravz --progress ${STORAGE}/${FASTQ} .
-
-# Quality Control
-    
-    NanoPlot --fastq ${FASTQ} -o  qc_report -t 16
-
-# Assembly
-    
-    flye --nano-hq ${FASTQ} -o assembly -t 16
-
-# Polishing
-    
-    medaka_consensus -i ${FASTQ} -d assembly/assembly.fasta -m r1041_e82_400bps_sup_v5.2.0 \
-         -o medaka_out -t 16
-
-########Transfert the contigs to the storage
-rsync -ravz --progress medaka_out/consensus.fasta  ${ASSEMBLIES}/${BARCODE}_contig.fasta
-
-    echo "Analysis $BARCODE completed"
+    echo "Analysis barcode$i completed"
 done
 
+rsync -ravz --progress barcode$i/medaka_out/consensus.fasta  ${ASSEMBLIES}/barcode$i.contig.fasta
